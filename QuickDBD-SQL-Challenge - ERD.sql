0a6dda6c0995b920f24d9e26dbcecf57ca6a47a4
kp-priest <kp@heykp.ninja>
2024-11-17 20:45:52 -0600

    adding ERD for sql-challenge

---
+++ QuickDBD-SQL-Challenge - ERD.sql
@@ -0,0 +1,66 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/BgERec
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" int   NOT NULL,
+    "birth_date" dateTime   NOT NULL,
+    "first_name" string   NOT NULL,
+    "last_name" string   NOT NULL,
+    "sex" string   NOT NULL,
+    "hire_date" dateTime   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_no" int   NOT NULL,
+    "dept_name" string   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" int   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" int   NOT NULL,
+    "title" string   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "emp_title_id" (
+    "emp_no" int   NOT NULL,
+    "title_id" int   NOT NULL,
+    CONSTRAINT "pk_emp_title_id" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
