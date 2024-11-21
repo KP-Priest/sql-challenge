f7f88147adae7d1844d5a830b9f79b49ed28e07a
kp-priest <kp@heykp.ninja>
2024-11-20 20:10:28 -0600

    Adding final version of analyssis.sql and update README file

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sql-challenge
 Mod 9 SQL Challenge
+
+Sites used to solve problems:
+
+
+
+https://neon.tech/postgresql/postgresql-tutorial/postgresql-like
+
+https://www.dbvis.com/thetable/how-to-join-three-or-more-tables-with-sql/#:~:text=To%20join%20three%20or%20more%20tables%20using%20a%20subquery%2C%20you,in%20the%20outer%20SELECT%20statement.
+
+https://stackoverflow.com/questions/23335970/postgresql-query-between-date-ranges
+
+https://stackoverflow.com/questions/63095867/how-to-get-the-frequency-in-postgresql
--- analysis.sql
+++ analysis.sql
@@ -1,4 +1,4 @@
---#1
+--#1 - List the employee number, last name, first name, sex, and salary of each employee.
 
 SELECT
 employees.emp_no,employees.last_name, employees.first_name, employees.sex, salaries.salary
@@ -10,7 +10,7 @@ INNER JOIN salaries ON employees.emp_no = salaries.emp_no
 
 ;
 
---#2
+--#2 - List the first name, last name, and hire date for the employees who were hired in 1986.
 
 SELECT
 employees.first_name, employees.last_name, employees.hire_date
@@ -21,7 +21,9 @@ employees
 WHERE hire_date BETWEEN '1/1/86' AND '12/31/86'
 ;
 
---#3
+-- #3 - List the manager of each department along with their: 
+-- 1)department number - 2) department name - 3) employee number - 4) last name - 5) first name
+  
 
 SELECT 
 dept_manager.dept_no, 
@@ -41,7 +43,7 @@ LEFT JOIN employees ON dept_manager.emp_no = employees.emp_no
 ;
 
 
---#4
+--#4 - List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 
 SELECT 
 
@@ -59,7 +61,8 @@ LEFT JOIN employees ON dept_emp.emp_no = employees.emp_no
 LEFT JOIN departmentS ON dept_emp.dept_no = departments.dept_no
 ;
 
---#5
+--#5 - List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
 SELECT employees.first_name, employees.last_name, employees.sex
 
 FROM employees
@@ -71,7 +74,25 @@ AND
 employees.last_name LIKE 'B%'
 ;
 
---#6
+--#6 - List each employee in the Sales department, including their employee number, last name, and first name.
+
+SELECT 
+dept_emp.emp_no,
+employees.last_name,
+employees.first_name
+
+FROM dept_emp
+
+INNER JOIN departments
+on dept_emp.dept_no = departments.dept_no
+
+INNER JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+
+WHERE departments.dept_name = 'Sales'
+;
+
+--#7 - List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 SELECT 
 employees.emp_no,
@@ -93,6 +114,19 @@ WHERE departments.dept_name = 'Sales'
 or departments.dept_name = 'Development'
 ;
 
+
+--#8 - List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+
+SELECT
+employees.last_name,
+COUNT (last_name) AS "Count of Last Name"
+
+FROM employees
+
+GROUP BY employees.last_name
+ORDER BY "Count of Last Name" DESC
+;
 
 
 
